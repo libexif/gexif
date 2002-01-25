68d3fe5ece74ed04f99f0ec5460d7cf2caca736b
Lutz Mueller <lutz.s.mueller@gmail.com>
2002-01-25 15:25:34 +0100

    New.

--- gexif/gexif-main.c
+++ gexif/gexif-main.c
@@ -32,12 +32,30 @@
 #include <gtk/gtkfilesel.h>
 #include <gtk/gtkbutton.h>
 
-#include <libexif/exif-i18n.h>
 #include <libjpeg/jpeg-data.h>
 
 #include <libexif-gtk/gtk-exif-browser.h>
 
 #include "gexif-thumbnail.h"
+
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  undef _
+#  define _(String) dgettext (PACKAGE, String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
+#else
+#  define textdomain(String) (String)
+#  define gettext(String) (String)
+#  define dgettext(Domain,Message) (Message)
+#  define dcgettext(Domain,Message,Type) (Message)
+#  define bindtextdomain(Domain,Directory) (Domain)
+#  define _(String) (String)
+#  define N_(String) (String)
+#endif
 
 struct _GExifMainPrivate
 {
